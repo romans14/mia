9fec9a3b32e706cecb69aeee759547ce169e4932
romans14 <272507@studenti.unimore.it>
2026-01-30 10:25:36 +0100

    Fix combined slip sign handling

--- TTC_data_analysis/ellisse_di_aderenza_finale.m
+++ TTC_data_analysis/ellisse_di_aderenza_finale.m
@@ -201,16 +201,16 @@ function [Fx, Fy] = MF_PAC2002_Fxy_combined(kappa,alpha,Fz,gamma,P,p)
     mu_x_max = Dx ./ Fz;
     mu_y_max = Dy ./ Fz;
 
-    mu_x_act_safe = mu_x_act;
-    mu_y_act_safe = mu_y_act;
-    mu_x_act_safe(abs(mu_x_act_safe) < eps) = eps;
-    mu_y_act_safe(abs(mu_y_act_safe) < eps) = eps;
+    mu_x_act_abs = abs(mu_x_act);
+    mu_y_act_abs = abs(mu_y_act);
+    mu_x_act_abs(mu_x_act_abs < eps) = eps;
+    mu_y_act_abs(mu_y_act_abs < eps) = eps;
 
-    mu_x = 1 ./ sqrt((1 ./ mu_x_act_safe).^2 + (tan(beta) ./ (mu_y_max + eps)).^2);
-    mu_y = tan(beta) ./ sqrt((1 ./ (mu_x_max + eps)).^2 + (tan(beta) ./ mu_y_act_safe).^2);
+    mu_x = 1 ./ sqrt((1 ./ mu_x_act_abs).^2 + (tan(beta) ./ (mu_y_max + eps)).^2);
+    mu_y = tan(beta) ./ sqrt((1 ./ (mu_x_max + eps)).^2 + (tan(beta) ./ mu_y_act_abs).^2);
 
-    Fx = (mu_x ./ mu_x_act_safe) .* Fx0;
-    Fy = (mu_y ./ mu_y_act_safe) .* Fy0;
+    Fx = (mu_x ./ mu_x_act_abs) .* Fx0;
+    Fy = (mu_y ./ mu_y_act_abs) .* Fy0;
 end
 
 function Gx = MF_PAC2002_Gx(kappa,alpha,Fz,gamma,P,p)
